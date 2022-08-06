c46eae4f4b02fa8c6607606ce2810ee9d58d934c
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-08-06 12:44:13 +0300

    Solution of the second example

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.WriteLine("Введите три числа, чтобы узнать какое из трех чисел максимальное");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.WriteLine(a + " максимальное число");
+}
+else if (b > a && b > c)
+{
+    Console.WriteLine(b + " максимальное число");
+}
+else
+{
+    Console.WriteLine(c + " максимальное число");    
+}
