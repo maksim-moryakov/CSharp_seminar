4f90057546e2801528e54a6c41cb8129e50973b2
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-08-15 23:21:46 +0300

    Решение 25 задачи, 4-го семинара

---
+++ Seminar4/Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int exponentiation (int a, int b) 
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите два числа А и В:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"Результатом возведения числа А в натуральную стпень В равно: {exponentiation(a, b)}");
