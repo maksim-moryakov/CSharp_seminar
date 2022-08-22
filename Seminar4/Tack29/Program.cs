c33bfa7936fb39893fae3e7065bcbfaa6976ee69
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-08-22 21:38:27 +0300

    Решение задач 27 и 29

---
+++ Seminar4/Tack29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void ArrayRandom(){
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.Length; i++){
+        arr[i] = new Random().Next(1,100);
+    }
+    Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
+
+ArrayRandom();
+Console.WriteLine();
---
+++ Seminar4/Task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int number)
+{
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = number - number % 10;
+        result = result + (number - advance);
+        number = number / 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumber = Sum(number);
+Console.WriteLine($"Сумма цифр: {sumNumber}");
