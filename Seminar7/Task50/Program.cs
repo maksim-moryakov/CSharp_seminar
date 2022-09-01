85a1163f5d213ad48999f26c5bedc35b943505b8
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-09-01 13:25:38 +0300

    Решение задач 7 семинара

---
+++ Seminar7/Task50/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число, и возвращает индексы этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] FillArray(int rows, int cols)
+{
+    int[,] array = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetUpperBound(0) + 1;
+    int columns = array.GetUpperBound(1) + 1;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = FillArray(4, 4);
+
+PrintArray(array);
+
+Console.WriteLine("Введите номер строки:");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер колонки:");
+int col = Convert.ToInt32(Console.ReadLine());
+
+if (row - 1 < array.GetLength(0) && col - 1 < array.GetLength(1))
+{
+    Console.WriteLine($"Индекс заданого элемента: {array[row - 1, col - 1]}");
+}
+else
+{
+    Console.WriteLine("Такого элемента в массиве нет");
+}
---
+++ Seminar7/Task52/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите количество строк массива: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int col = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int[row, col];
+
+for (int i = 0; i < row; i++)
+{
+    for (int  j = 0; j < col; j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine("");
+}
+
+Console.WriteLine();
+Console.Write($"Cреднее арифметическое каждого столбца: ");
+for (int i = 0; i < col; i++)
+{
+  double average = 0;
+  for (int j = 0; j < row; j++)
+  {
+    average += array[j, i];
+  }
+  average = average / row;
+  Console.Write($"{average} ");
+}
