e17841f01084e936f3ea74d0ae8b3ca679910331
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-09-05 22:20:56 +0300

    Задачи 8 семинара

---
+++ Seminar8/Task58/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Clear();
+Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+int[,] firstMartrix = new int[m, n];
+Console.WriteLine($"\nПервая матрица:");
+FillArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+Console.WriteLine($"\nВторая матрица:");
+FillArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m, p];
+
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secomdMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+            Console.Write(resultMatrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Seminar8/Task60/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,0,1)
+// 34(0,1,0) 41(0,1,1)
+// 27(1,0,0) 90(1,0,1)
+// 26(1,1,0) 55(1,1,1)
+
+Console.Clear();
+Console.Write("Введите размер трёхмерного массива [X, Y, Z]: ");
+Console.WriteLine();
+int x = InputNumbers("Введите X: ");
+int y = InputNumbers("Введите Y: ");
+int z = InputNumbers("Введите Z: ");
+
+int[,,] Create3DArray(int size1, int size2, int size3)
+{
+    int[,,] array = new int[size1, size2, size3];
+    int[] values = new int[89];
+    int num = 10;
+    for (int i = 0; i < values.Length; i++)
+        values[i] = num++;
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        int randomInd = new Random().Next(0, values.Length);
+        int temp = values[i];
+        values[i] = values[randomInd];
+        values[randomInd] = temp;
+    }
+
+    int valueIndex = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++) 
+            {
+                array[i, j, k] = values[valueIndex++];
+                Console.WriteLine($"{array[i, j, k]} ({i},{j},{k})");
+            }
+            Console.WriteLine();
+        }
+    }
+    return array;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+Create3DArray(x, y, z);
+
