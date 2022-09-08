9be0e91eb0cf64792b17238e23e5f5c0b51247e3
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-09-08 16:25:29 +0300

    Решение задач 9 семинар

---
+++ Seminar9/Task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.WriteLine("Поиск натуральных чисел в промежутке от M до N: ");
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = RangeOfNumbers(m, n);
+Console.WriteLine($"M = {m}; N = {n}. - > {result}");
+
+string RangeOfNumbers(int m, int n)
+{
+    if (m > n)
+    {
+        return RangeOfNumbers(m, n + 1) + ", " + n;
+    }
+    else if (m < n)
+    {
+        return RangeOfNumbers(m + 1, n) + ", " + m;
+    }
+    {
+        return Convert.ToString(n);
+    }
+}
---
+++ Seminar9/Task68/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+Console.Write("Введи M: ");
+int m = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи N: ");
+int n = Convert.ToInt16(Console.ReadLine());
+Ackerman(n, m);
+            
+static int Ackerman(int n, int m)
+{
+    if (n == 0) 
+    {
+        return m + 1;
+    }    
+    if (n != 0 && m == 0) 
+    {
+        return Ackerman(n - 1, 1);
+    }    
+    if (n > 0 && m > 0) 
+    {
+        return Ackerman(n - 1, Ackerman(n, m - 1));
+    }    
+    return Ackerman(n,m);
+}
+Console.WriteLine(Ackerman(n,m));
